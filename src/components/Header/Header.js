a8910478ef8228d0873e3f4e2f568fde02486220
Denys Malykhin <denys.malykhin@solidopinion.com>
2019-09-21 18:03:39 +0300

    machine reset

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { array } from 'prop-types'
 import { get } from 'lodash'
+import cn from 'classnames'
 
-import { useSubscription } from '@apollo/react-hooks'
+import { useSubscription, useMutation } from '@apollo/react-hooks'
 
-import { Navbar, Alignment } from '@blueprintjs/core'
+import { Navbar, Alignment, Button, Classes } from '@blueprintjs/core'
 
 import NavItem from './NavItem'
 import Status from './Status/Status'
 
 import MACHINE_STATUS_SUBSCRIPTION from 'subscriptions/machineStatus.graphql'
+import RESET_MACHINE_MUTATION from 'mutations/resetMachine.graphql'
 
 const Header = ({ routes }) => {
   const { data } = useSubscription(MACHINE_STATUS_SUBSCRIPTION)
   const machineStatus = get(data, 'machineStatus.statusName')
 
+  const [resetMachine, { loading }] = useMutation(RESET_MACHINE_MUTATION)
+
+  const isLoading = loading
   return (
     <Navbar>
       <Navbar.Group align={Alignment.LEFT}>
@@ -26,6 +31,9 @@ const Header = ({ routes }) => {
         ))}
       </Navbar.Group>
       <Navbar.Group align={Alignment.RIGHT}>
+        <Button onClick={resetMachine} className={cn({ [Classes.SKELETON]: isLoading })}>
+          Reset
+        </Button>
         <Navbar.Divider />
         <Navbar.Heading>
           <Status machineStatus={machineStatus} />
